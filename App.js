0505697d73a4dcf28426bda4fb06d93d707a1373
kemal bgl <kemalbgl@hotmail.com>
2020-03-12 20:40:55 +0300

    Appjs Deneme Kemal

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Kemalcim selam!</Text>
+      <Text>Corono Morona!</Text>
     </View>
   );
 }
