2f59e0c17cb281a782236766740462f920e6486b
kemal bgl <kemalbgl@hotmail.com>
2020-03-12 20:45:06 +0300

    AppJs Deneme Kemal-2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Kemalcim selam!</Text>
+      <Text>Corona günlerinde aşk!</Text>
     </View>
   );
 }
